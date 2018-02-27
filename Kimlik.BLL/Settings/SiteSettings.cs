c3b46b19729f8b9f0ab727291879ae81318c58f9
Murat Bircan <mbircan9@gmail.com>
2018-02-27 19:32:52 +0200

    27.02.2018

--- Kimlik.BLL/Settings/SiteSettings.cs
+++ Kimlik.BLL/Settings/SiteSettings.cs
@@ -1,13 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
+using Kimlik.Models.ViewModels;
 
 namespace Kimlik.BLL.Settings
 {
     public static class SiteSettings
     {
+        public static string SiteMail = "kamilfidil@gmail.com";
+        public static string SiteMailPassword = "123456kf";
+        public static string SiteMailSmtpHost = "smtp.gmail.com";
+        public static int SiteMailsmtpPort = 587;
+        public static bool SiteMailEnableSsl = true;
+
+        public static async Task SendMail(MailModel model)
+        {
+            using (var smtp=new SmtpClient())
+            {
+                var message=new MailMessage();
+                message.To.Add(model.To);
+                message.From=new MailAddress(SiteMail);
+                message.Subject = model.Subject;
+                message.SubjectEncoding=Encoding.UTF8;
+                message.IsBodyHtml = true;
+                message.BodyEncoding=Encoding.UTF8;
+                message.Body = model.Message;
+                if(!string.IsNullOrEmpty(model.Cc))
+                    message.CC.Add(model.Cc);
+                if (!string.IsNullOrEmpty(model.Bcc))
+                    message.Bcc.Add(model.Bcc);
+                var credential = new NetworkCredential
+                {
+                    UserName = SiteMail,
+                    Password = SiteMailPassword
+                };
+                smtp.Credentials = credential;
+                smtp.Host = SiteMailSmtpHost;
+                smtp.Port = SiteMailsmtpPort;
+                smtp.EnableSsl = SiteMailEnableSsl;
+                await smtp.SendMailAsync(message);
+            }
+        }
         public static string UrlFormatConverter(string name)
         {
             string sonuc = name.ToLower();
--- Kimlik.Web.MVC/Controllers/AccountController.cs
+++ Kimlik.Web.MVC/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Kimlik.BLL.Account;
+using Kimlik.BLL.Settings;
 using Kimlik.Models.IdentityModels;
 using Kimlik.Models.ViewModels;
 using Microsoft.AspNet.Identity;
@@ -88,6 +89,126 @@ namespace Kimlik.Web.MVC.Controllers
         {
             HttpContext.GetOwinContext().Authentication.SignOut();
             return RedirectToAction("Index", "Home");
+        }
+        [Authorize]
+        public async Task<ActionResult> Profile()
+        {
+            var userManeger = NewUserManager();
+            var user = await userManeger.FindByIdAsync(HttpContext.User.Identity.GetUserId());
+            var model = new ProfilePasswordMultiViewModel();
+            var modelProfile = new ProfileViewModel()
+            {
+                Email = user.Email,
+                Name = user.Name,
+                Surname = user.Surname,
+                UserName = user.UserName,
+                RegisterDate = user.RegisterDate
+            };
+            model.ProfileViewModel = modelProfile;
+            return View(model);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Profile(ProfilePasswordMultiViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            try
+            {
+                var userStore = NewUserStore();
+                var userManager = new UserManager<ApplicationUser>(userStore);
+                var user = await userManager.FindByIdAsync(HttpContext.User.Identity.GetUserId());
+                user.Name = model.ProfileViewModel.Name;
+                user.Surname = model.ProfileViewModel.Surname;
+                user.Email = model.ProfileViewModel.Email;
+                await userStore.UpdateAsync(user);
+                await userStore.Context.SaveChangesAsync();
+                return RedirectToAction("Profile", "Account");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("",$"Güncelleme sırasında bir hata oluştu.{ex.Message}");
+                return View("Profile");
+            }
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> ChangePassword(ProfilePasswordMultiViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return RedirectToAction("Profile","Account");
+            try
+            {
+                var userStore = NewUserStore();
+                var userManager = new UserManager<ApplicationUser>(userStore);
+                var user = await userManager.FindByIdAsync(HttpContext.User.Identity.GetUserId());
+                user = await userManager.FindAsync(user.UserName, model.ChangePasswordViewModel.OldPassword);
+                if (user == null)
+                {
+                    ModelState.AddModelError("OldPassword", "Mevcut şifreniz hatalı");
+                    return View("Profile");
+                }
+                await userStore.SetPasswordHashAsync(user,
+                    userManager.PasswordHasher.HashPassword(model.ChangePasswordViewModel.Password));
+                await userStore.UpdateAsync(user);
+                await userStore.Context.SaveChangesAsync();
+                HttpContext.GetOwinContext().Authentication.SignOut();
+                return RedirectToAction("Profile");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("",$"Güncelleme işleminde bir hata oluştu.{ex.Message}");
+                return View("Profile");
+            }
+        }
+
+        public ActionResult RecoverPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> RecoverPassword(RecoverPasswordViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            var userStore = NewUserStore();
+            var userManager = new UserManager<ApplicationUser>(userStore);
+            var user1 = await userManager.FindByEmailAsync(model.Email);
+            if (user1 == null)
+            {
+                ModelState.AddModelError("","Bu e-mail adresi kayıtlı değildir.");
+                return View(model);
+            }
+
+            var user2 = await userManager.FindByNameAsync(model.Username);
+            if (user2 == null)
+            {
+                ModelState.AddModelError("", "Bu kullanıcı adı değildir.");
+                return View(model);
+            }
+
+            if (user1.Id != user2.Id)
+            {
+                ModelState.AddModelError("", "Girdiğiniz bilgiler geçerli değil");
+                return View(model);
+            }
+
+            var randomPassword = Guid.NewGuid().ToString().Replace("-", "").Substring(0, 6);
+            await userStore.SetPasswordHashAsync(user1, userManager.PasswordHasher.HashPassword(randomPassword));
+            await userStore.Context.SaveChangesAsync();
+            await SiteSettings.SendMail(new MailModel()
+            {
+                To = user1.Email,
+                Subject = "Şifreniz değişti",
+                Message = $"Merhaba {user1.Name} <br>Yeni Şifreniz <b>{randomPassword}</b>"
+            });
+            return RedirectToAction("Login", "Account");
         }
     }
 }
