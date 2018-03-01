0b6516d116ea33cbd4cf84ed51e96e0a4a75af92
Murat Bircan <mbircan9@gmail.com>
2018-03-01 19:14:01 +0200

    01.03.2018-fixed

--- NorthRestAPI/Models/ViewModels/ProductViewModel.cs
+++ NorthRestAPI/Models/ViewModels/ProductViewModel.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace NorthRestAPI.Models.ViewModels
 {
     public class ProductViewModel
     {
+        [Required]
         public string ProductName { get; set; }
-        public int CategoryID { get; set; }
+        public int? CategoryID { get; set; }
         public decimal UnitPrice { get; set; }
         public short UnitsInStock { get; set; }
     }
